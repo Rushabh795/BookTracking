024e513c704d961fa94d4321e9a2fa2c8630b75b
Rushabh Shah <rushabh795@gmail.com>
2022-08-14 21:02:38 -0400

    Add migration

--- BookTrack/BookTrack/Models/Book.cs
+++ BookTrack/BookTrack/Models/Book.cs
@@ -7,6 +7,7 @@ namespace BookTrack.Models
         public int ID { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
+ 
         [Range(1, 10, ErrorMessage = "Rating should be between 1 to 10")]
         public int Rating { get; set; }
     }
