28eb0b83d23734184c342cce86d3313bcf372a9c
Rushabh Shah <rushabh795@gmail.com>
2022-08-14 21:09:23 -0400

    Book Scafflding Add

---
+++ Book-Tracking_v6/Book-Tracking_v6/Pages/book/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Book_Tracking_v6.Models;
+using Bookcatelog.Data;
+
+namespace Book_Tracking_v6.Pages.book
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly Bookcatelog.Data.BookCatalogContext _context;
+
+        public DeleteModel(Bookcatelog.Data.BookCatalogContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+      public Book Book { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Book == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Book.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Book = book;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.Book == null)
+            {
+                return NotFound();
+            }
+            var book = await _context.Book.FindAsync(id);
+
+            if (book != null)
+            {
+                Book = book;
+                _context.Book.Remove(Book);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
