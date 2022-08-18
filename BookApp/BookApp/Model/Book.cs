7c16ad0103436e144ffb2a855ee19becf6974af3
Rushabh Shah <rushabh795@gmail.com>
2022-08-18 11:40:21 -0400

    Added three models

---
+++ BookApp/BookApp/Model/Book.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace BookApp.Model
+{
+    public class Book
+    {
+        public Book()
+        {
+        }
+    }
+}
+
---
+++ BookApp/BookApp/Model/Category.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace BookApp.Model
+{
+    public class Category
+    {
+        public Category()
+        {
+        }
+    }
+}
+
